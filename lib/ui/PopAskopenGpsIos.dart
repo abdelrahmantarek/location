9a4e4a2506e601ab62b0411995c4a53d552dfdd9
abdulrahman tarek <31620727+abdelrahmantarek@users.noreply.github.com>
2022-06-26 12:11:55 +0200

    Update PopAskopenGpsIos.dart

--- lib/ui/PopAskopenGpsIos.dart
+++ lib/ui/PopAskopenGpsIos.dart
@@ -6,27 +6,38 @@ import 'package:flutter/material.dart';
 class PopAskOpenGpsIos extends StatelessWidget {
   final Function? onCancel;
   final Function? onGoSettings;
-  const PopAskOpenGpsIos({Key? key, this.onCancel, this.onGoSettings}) : super(key: key);
+  final String gpsTitle;
+  final String gpsSubtitle;
+  final String cancelText;
+  final String settingsText;
+  const PopAskOpenGpsIos({Key? key,
+    this.onCancel,
+    this.onGoSettings,
+    required this.gpsTitle,
+    required this.gpsSubtitle,
+    required this.cancelText,
+    required this.settingsText
+  }) : super(key: key);
   @override
   Widget build(BuildContext context) {
     return CupertinoAlertDialog(
-      title: const Text('خدمات الموقع'),
+      title:  Text(gpsTitle),
       content: SingleChildScrollView(
         child: Column(
-          children: const <Widget>[
-            Text('للاستمرار يجب عليك تفعيل خدمات الموقع من الاعدادات / الخصوصية / خدمات الموقع'),
+          children: <Widget>[
+            Text(gpsSubtitle),
           ],
         ),
       ),
       actions: <Widget>[
         TextButton(
-          child: const Text('الاعدادات'),
+          child:  Text(settingsText),
           onPressed: () {
             onGoSettings!();
           },
         ),
         TextButton(
-          child: const Text('الغاء'),
+          child:  Text(cancelText),
           onPressed: () {
             onCancel!();
           },
